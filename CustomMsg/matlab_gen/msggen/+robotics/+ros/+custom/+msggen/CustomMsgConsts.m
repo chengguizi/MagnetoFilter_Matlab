15c695383f9c117cd0652a21bf326faf5d908f4b
chengguizi <cgz_05@sina.com>
2017-10-02 12:03:42 +0800

    First Commit

---
+++ CustomMsg/matlab_gen/msggen/+robotics/+ros/+custom/+msggen/CustomMsgConsts.m
@@ -0,0 +1,56 @@
+classdef CustomMsgConsts
+    %CustomMsgConsts This class stores all message types
+    %   The message types are constant properties, which in turn resolve
+    %   to the strings of the actual types.
+    
+    %   Copyright 2014-2017 The MathWorks, Inc.
+    
+    properties (Constant)
+        ssf_core_visensor_imu = 'ssf_core/visensor_imu'
+        visensor_node_visensor_imu = 'visensor_node/visensor_imu'
+    end
+    
+    methods (Static, Hidden)
+        function messageList = getMessageList
+            %getMessageList Generate a cell array with all message types.
+            %   The list will be sorted alphabetically.
+            
+            persistent msgList
+            if isempty(msgList)
+                msgList = cell(2, 1);
+                msgList{1} = 'ssf_core/visensor_imu';
+                msgList{2} = 'visensor_node/visensor_imu';
+            end
+            
+            messageList = msgList;
+        end
+        
+        function serviceList = getServiceList
+            %getServiceList Generate a cell array with all service types.
+            %   The list will be sorted alphabetically.
+            
+            persistent svcList
+            if isempty(svcList)
+                svcList = cell(0, 1);
+            end
+            
+            % The message list was already sorted, so don't need to sort
+            % again.
+            serviceList = svcList;
+        end
+        
+        function actionList = getActionList
+            %getActionList Generate a cell array with all action types.
+            %   The list will be sorted alphabetically.
+            
+            persistent actList
+            if isempty(actList)
+                actList = cell(0, 1);
+            end
+            
+            % The message list was already sorted, so don't need to sort
+            % again.
+            actionList = actList;
+        end
+    end
+end
