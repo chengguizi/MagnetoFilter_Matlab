c0c766bf8ac53263cb61d6c72c54f075c57162f6
chengguizi <cgz_05@sina.com>
2017-10-03 18:09:01 +0800

    Fix jumping glitch at boundary, by holding Z value transition back

--- EKF_main.m
+++ EKF_main.m
@@ -1,5 +1,5 @@
 
-
+close all
 filePath = '/home/dhl/Desktop/mag/2017-10-02-14-26-44.bag';
 
 if ~exist('matx')
--- EKFupdate.m
+++ EKFupdate.m
@@ -12,12 +12,13 @@ function [X_next , P_next] = EKFupdate(X,P,U,Z,t_delta)
     F = [1  t_delta ; 0  1 ];
     B = [ 0 ; t_delta];
     X_hat =  F*X + B*U;
+    X_hat(1) = mod ( X_hat(1), 2*pi);
     
     
     % P_hat = G*P*G.' + Q
     % G is local-linearised F
     % for this model, F is actually linear
-    Q = 0.001*eye(2);
+    Q = [ 1e-3 , 0 ; 0 , 1e-7];
     P_hat = F*P*(F.') + Q;
     
     % H is the transformation from sensor reading to state
@@ -26,11 +27,22 @@ function [X_next , P_next] = EKFupdate(X,P,U,Z,t_delta)
     H = [ 1 0 ]; % direct feeding of theta to expected Z
     
     % Karman Gain
-    R = 0.05;
+    R = 0.005;
     K = P_hat*(H.')*inv(H*P_hat*(H.') + R);
     
-    
-    X_next = X_hat + K*( Z - H*X_hat );
+    Z_expected = H*X_hat;
+    % if Z jumps at boundary condition at 0 or 360 degree
+    if ( abs(Z - Z_expected) > pi/2)
+       
+        if (Z > Z_expected)
+            Z = Z - 2*pi;
+        else
+            Z = Z + 2*pi;
+        end
+        
+    end
+     
+    X_next = X_hat + K*( Z - Z_expected );
     X_next (1) = mod (X_next (1),2*pi);
     
     P_next = P_hat - K*H*P_hat;
